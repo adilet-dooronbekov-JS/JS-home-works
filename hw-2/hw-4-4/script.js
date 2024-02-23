4d05fba3984da8544540e49639945f24d6c0bb27
adilet-dooronbekov-JS <qooyda@gmail.com>
2024-02-23 17:49:25 +0600

    add hw-4-4

---
+++ hw-2/hw-4-4/script.js
@@ -0,0 +1,33 @@
+function showMessage(one, two) {
+  console.log("Hello world");
+}
+
+showMessage();
+
+//
+
+function calcFunction(hi, ther) {
+  return hi + ther;
+}
+console.log(calcFunction(77, 33));
+
+//
+
+function mySquare(number) {
+  return number * number;
+}
+console.log(mySquare("10"));
+
+//
+
+function myName(hello, name) {
+  return hello + name;
+}
+
+console.log(myName("Wellcom", "Adilet"));
+//
+function myN(a, b) {
+  return a + b;
+}
+
+console.log(myN("Hellow", "world"));
